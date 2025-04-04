bf8b1c01a359f4dc929bce5d9d30b7cdb168bdfd
DeepakG45 <sharama445@gmail.com>
2025-04-04 16:44:04 +0530

    Fix: Updated Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  base: "/",
+  base: "./", // Ensures correct paths in production
   build: {
     outDir: "dist",
-  },
-  server: {
-    port: 3000,
-  },
+  }
 });
